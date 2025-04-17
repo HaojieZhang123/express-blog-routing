cd06ffd9b865ebff87df401465ca61046fc31e83
HaojieZhnag123 <haojiezhnag123@gmail.com>
2025-04-17 15:27:05 +0200

    [UPD] basic initialization on app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Simple Express.js application that serves static files from the "public" directory and provides a RESTful API endpoint to get a list of items.
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+// base route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// server listens on port 3000
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
