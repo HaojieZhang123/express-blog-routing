d4668a688225791123807ceeeb55daf16bd0208d
HaojieZhnag123 <haojiezhnag123@gmail.com>
2025-04-17 15:37:22 +0200

    [UPD] public folder created. moved static assets into public folder. created routing file routers/posts.js

---
+++ routers/posts.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+// router class
+const router = express.Router();
+
+// get all posts
+router.get('/', (req, res) => {
+    res.send('Lista dei post');
+});
+
+// get post by id
+router.get('/:id', (req, res) => {
+    const postId = req.params.id;
+    res.send(`Post con id ${postId}`);
+});
+
+// create a new post
+router.post('/', (req, res) => {
+    res.send(`Creazione nuovo post`);
+});
+
+// update post by id
+router.put('/:id', (req, res) => {
+    const postId = req.params.id;
+    res.send(`Post con id ${postId} aggiornato`);
+});
+
+// modify post by id
+router.patch('/:id', (req, res) => {
+    const postId = req.params.id;
+    res.send(`Post con id ${postId} modificato`);
+});
+
+// delete post by id
+router.delete('/:id', (req, res) => {
+    const postId = req.params.id;
+    res.send(`Post con id ${postId} eliminato`);
+});
+
+// export the router
+module.exports = router;
